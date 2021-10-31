06486df0b39003e69f8a14b192fcafa7804612fb
Muhammad Umar <uali10772@gmail.com>
2021-10-31 21:39:55 +0500

    Time conversion method added

--- bot/tidal.py
+++ bot/tidal.py
@@ -29,9 +29,22 @@ class Tidal:
                 EC.presence_of_element_located((by, tag))
             )
         except Exception as e:
-            print("Element not found. Name: ", tag)
+            print('Element not found. Name: ', tag)
         finally:
             return element
+
+    def time_to_sec(self, time_str):
+        time_hms = [ int(i) for i in time_str.split(':')]
+        if len(time_hms) == 2:
+            return time_hms[0] * 60 + time_hms[1]
+        elif len(time_hms) == 1:
+            return time_hms[0]
+        elif len(time_hms) == 3:
+            return time_hms[0] * 3600 + time_hms[1] * 60 + time_hms[0]
+        return None
+
+    def get_song_random_point(self):
+        song_duration = self.get_time_duration().split(':')
 
     def __enter_username(self):
         element = self.__wait_tag_by_sec('email', By.ID, 10)
@@ -49,10 +62,18 @@ class Tidal:
         element = self.__wait_tag_by_sec('recap-invisible', By.ID, 10)
         element.click()
 
+    def is_blocked(self):
+        "document.body.innerText"
+        element = self.__wait_tag_by_sec('iframe', By.TAG_NAME, 5)
+        if element:
+            if element.get_attribute('height') == '100%' or self.browser.execute_script('document.body.innerText') == None:
+                return True
+        return False
+
     def __perform_login(self, login_btn):
         
         login_btn.click()
-        time.sleep(10)
+        time.sleep(5)
         self.__enter_username()
         time.sleep(5)
         self.__press_login_continue_btn()
@@ -82,6 +103,11 @@ class Tidal:
         element = self.__wait_tag_by_sec(btn, By.XPATH, 10)
         return element.get_attribute('textContent')
 
+    def get_current_time(self):
+        btn = "//time[@data-test='current-time']"
+        element = self.__wait_tag_by_sec(btn, By.XPATH, 10)
+        return element.get_attribute('textContent')
+
     def pause_song(self):
         btn = "//button[@data-test='pause']"
         element = self.__wait_tag_by_sec(btn, By.XPATH, 10)
@@ -93,14 +119,14 @@ class Tidal:
         element.click()
 
     def __login_check(self):
-        element = self.__wait_tag_by_sec("login-button", By.ID, 5)
+        element = self.__wait_tag_by_sec('login-button', By.ID, 5)
         if element:
             try:
                 time.sleep(5)
                 self.__perform_login(element)
                 return True
             except Exception as e:
-                print("Unable to login. Error! ", e)
+                print('Unable to login. Error! ', e)
                 return False
         return True
 
@@ -109,7 +135,7 @@ class Tidal:
 
     def setup(self):
         self.__get()
-        time.sleep(5)
+        time.sleep(10)
         login_success = self.__login_check()
 
         if login_success:
--- main.py
+++ main.py
@@ -1,17 +1,32 @@
+from random import randrange
 from bot.tidal import Tidal
 import undetected_chromedriver.v2 as driver
 
 
+def get_credentials(filename: str):
+    with open(filename, 'r') as file:
+        credentials_string = file.readlines()
+        credentials = [tuple(c.strip().split(':')) for c in credentials_string]
+    return credentials or []
+
 try:
-    browser = driver.Chrome()
-    tidal = Tidal(
-        browser,
-        "https://listen.tidal.com/artist/10003047",
-        "majesticeagle1@gmail.com",
-        "snob86dew",
-    )
+    options = driver.ChromeOptions()
+    PROXY = '23.107.176.19:32180'
+    # options.add_argument("--proxy-server=%s" % PROXY)
+    browser = driver.Chrome(options=options)
+    credentials = get_credentials('credentials.txt')
+
+    links = [
+        'https://listen.tidal.com/artist/10003047',
+        'https://listen.tidal.com/album/186907408',
+        'https://listen.tidal.com/playlist/fdf9201c-5e21-462f-a50f-33e31566ee74',
+    ]
+
+    tidal = Tidal(browser, links[0], credentials[0][0], credentials[0][1],)
     tidal.setup()
     tidal.stream_song()
+
+    
 except Exception as e:
     print(e)
 finally:
